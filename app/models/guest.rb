b4eb3d5675daa0ed442e930cee3fc0d24bf46558
suyuxp <suyu@zhiyisoft.com>
2013-04-04 16:21:33 +0800

    客户应用名称的合法性检查

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -6,4 +6,5 @@ class Guest
 
   validates_uniqueness_of :app, :name
   validates_presence_of :app, :name
+  validates_format_of :app, with: /^[a-z][a-z0-9\-]{2,30}[a-z0-9]$/
 end
--- spec/models/guest_spec.rb
+++ spec/models/guest_spec.rb
@@ -5,6 +5,13 @@ describe Guest do
 
   before(:each) do
     @example = { app: 'ccde-stmis', name: '科技管理系统' }
+    @invalids = [ { app: 'st3', name: '科技管理系统' },        # 应用名称太短
+                  { app: 'ccde第2版', name: '科技管理系统' }, # 应用名称中带中文，非法字符
+                  { app: '3ccde-stmis', name: '科技管理系统' }, # 不能以数字开头
+                  { app: '-ccde-stmis', name: '科技管理系统' }, # 不能以中划线开头
+                  { app: 'ccde-stmis-', name: '科技管理系统' }, # 不能以中划线结尾
+                  { app: 'ccde-stmis-ccde-stmis-ccde-stmis-ccde-stmis', name: '科技管理系统' }, # 过长，超过32位字符
+                ]
   end
 
   it "授权应用的注册" do
@@ -14,5 +21,11 @@ describe Guest do
   it "不允许重复注册" do
     Guest.create!(@example)
     expect { Guest.create!(@example) }.to raise_error
+  end
+
+  it "应用名过短或者不是字母、数字加下划线是不合法的" do
+    @invalids.each do |guest|
+      expect { Guest.create!(guest) }.to raise_error
+    end
   end
 end
